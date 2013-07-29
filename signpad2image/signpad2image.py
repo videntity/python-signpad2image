b6c54afc36bb8a308279c3b0b759da147024e2e5
Nick Meharry <nick@drchrono.com>
2013-07-29 11:31:10 -0700

    Fix typo

--- signpad2image/signpad2image.py
+++ signpad2image/signpad2image.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4
-from PiL import Image, ImageDraw
+from PIL import Image, ImageDraw
 import json, sys, os, exceptions
 
 try:
